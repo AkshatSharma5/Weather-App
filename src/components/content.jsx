875d5b0c71ebd86c0d7148b80b93b991ee423d25
AKSHAT SHARMA <akshatgopal70@gmail.com>
2024-05-29 20:38:06 +0530

    added error feature and keydown in input

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -3,14 +3,22 @@ import day from "../assets/dayy.gif";
 import night from "../assets/nighttt.gif"
 import { useRef, useState } from "react";
 import Weather from "./weather";
+
 function Content(){
-    const [city,setCity] = useState('Mathura');
+
+    const [city,setCity] = useState(`Mathura`);
     const [isDay,setIsDay] = useState(1);
 
     
     const handleOnClick = () => {
         setCity(document.getElementById('cityInput').value);
         // alert(document.getElementById('cityInput').value);
+    }
+    const handleOnKey = (event) => {
+        if (event.key === 'Enter') {
+            setCity(document.getElementById('cityInput').value);
+            // alert(document.getElementById('cityInput').value);
+        }
     }
     const isDayHandle=(dayvalue)=>{
         setIsDay(dayvalue===1);
@@ -22,10 +30,10 @@ function Content(){
             <div className={`sm:mt-[20vh] mt-[15vh] xl:mt-0 w-[90wh] h-[100vh] xl:overflow-hidden xl:w-[80vw] xl:h-[80vh] hue-rotate-15 bg-transparent pl-[15vw] xs:pl-[0vw] backdrop-blur-lg border border-white shadow-2xl ${isDay==1?"shadow-white":"shadow-sky-600"} rounded-xl font-official text-center`}>
                 <div className="flex flex-col justify-center items-center">
 
-                    <input type="text" id='cityInput' placeholder="Enter city" defaultValue='Mathura'  className="outline-none sm:p-3 w-[20vw] xs:w-[21vw] xl:text-3xl sm:text-2xl 
+                    <input type="text" id='cityInput' onKeyDown={handleOnKey} placeholder="Enter city" defaultValue='Mathura'  className="outline-none sm:p-3 w-[20vw] xs:w-[21vw] xl:text-3xl sm:text-2xl 
                     text-xl py-0.5 px-1.5 absolute  left-[3vw] top-[15vh] bg-transparent border border-white rounded-md text-white placeholder-zinc-50"/> 
 
-                    <button type="submit" onClick={handleOnClick} className={`text-white transition absolute sm:px-6 sm:py-2 px-[2vw] py-[1vh]  top-[28vh] left-[3vw] rounded-lg text-base sm:text-xl uppercase font-secon bg-gradient-to-tr from-green-400 to-sky-600 font-semithin active:from-pink-400 active:scale-105 active:translate-y-1 active:translate-x-1 active:to-yellow-600 `} >See Now</button>
+                    <button type="submit" onClick={handleOnClick} className={`text-white transition absolute sm:px-6 sm:py-2 px-[2vw] py-[1vh]  top-[28vh] left-[3vw] rounded-lg text-base sm:text-xl uppercase font-secon bg-gradient-to-tr from-green-400 to-sky-600 font-semithin active:from-pink-400 active:scale-105 active:translate-y-1 active:translate-x-1 active:to-yellow-600 active:transition-all`} >See Now</button>
                 </div>
                     <Weather city={city} isDayHandle={isDayHandle}/>
                     {/* <Weather/> */}
--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -152,6 +152,15 @@ const Weather = ({ city,isDayHandle }) => {
   ];
 
   
+  const [status,setStatus] = useState("200");
+  const url = `https://api.weatherapi.com/v1/current.json?key=40f03e2e9a2541c6981214138242003&q=${city}&aqi=no`;
+
+fetch(url)
+  .then(response => {
+    setStatus(response.status);
+  })
+
+
 
   useEffect(() => {
     const fetchWeather = async () => {
@@ -169,8 +178,16 @@ const Weather = ({ city,isDayHandle }) => {
     fetchWeather();
   }, [city]);
 
+  if(status==400){
+    return (
+      <div
+        className="absolute font-secon text-red-600 bg-white animate-bounce px-10 py-6 font-bold text-3xl top-1/3 left-[50%] bg-opacity-75 rounded-full"
+      >
+        Error 404! <br/> Please Enter correct Name!
+      </div>
+    );
+  }
   
-
   // Render weather data only if it's available -- imp.
   if (!weatherData) {
     return (
@@ -186,12 +203,12 @@ const Weather = ({ city,isDayHandle }) => {
   }
 
   const myDayHandle = (weatherData) => {
-    if(weatherData.current.is_day!=undefined) isDayHandle(weatherData.current.is_day);
+    if(weatherData?.current?.is_day!=undefined) isDayHandle(weatherData?.current?.is_day);
   }
 
   myDayHandle(weatherData);
   let codee=null;
-  let country_name=weatherData.location.country;
+  let country_name=weatherData?.location?.country;
   
   for(let i=0;i<Country.length;i++){
       if(Country[i].name===country_name) {codee=Country[i].code;break;}
@@ -210,25 +227,25 @@ const Weather = ({ city,isDayHandle }) => {
         <div
           key={i}
           className={`text-white uppercase cursor-pointer p-2.5 bg-gradient-to-tr
-           ${i === 0? weatherData.current[item.value] >= 22? `${`${hot} hover:from-green-500 hover:to-blue-500`}`: cold: ""} hover:from-pink-600 hover:to-yellow-600 bg-darken border-2 m-2 rounded-lg mt-5 py-4 xl:text-4xl lg:text-3xl sm:text-2xl xs:text-xl text-lg
+           ${i === 0? weatherData?.current[item?.value] >= 22? `${`${hot} hover:from-green-500 hover:to-blue-500`}`: cold: ""} hover:from-pink-600 hover:to-yellow-600 bg-darken border-2 m-2 rounded-lg mt-5 py-4 xl:text-4xl lg:text-3xl sm:text-2xl xs:text-xl text-lg
            ${i === 0 ? "xl:col-span-2 col-span-1" : ""} `}
         >
           <span className={`${i == 0 ? "visible absolute top-[10vh] left-[75vw] text-base font-mainn font-bold": "hidden"}`}>
             <span className="xl:inline-block hidden md:text-white lg:text-white ">
-              {weatherData.current[item.value] >= 22 ? "HOT" : "COLD"}
+              {weatherData?.current[item?.value] >= 22 ? "HOT" : "COLD"}
             </span>
           </span>
-          {item.label}
+          {item?.label}
           <div
-            className={`font-secon font-thin ${i == 1 ? `${(weatherData.current.condition.text.length<=8)?"xl:text-4xl lg:text-3xl text-2xl":"text-xl"}`:"xl:text-5xl lg:text-3xl text-2xl"}`}>
-            {i != 1? weatherData.current[item.value] : weatherData.current.condition.text}{" "}
-            <span className="text-xl ">{i != 1 ? item.unit : ""}</span>
+            className={`font-secon font-thin ${i == 1 ? `${(weatherData?.current?.condition?.text?.length<=8)?"xl:text-4xl lg:text-3xl text-2xl":"text-xl"}`:"xl:text-5xl lg:text-3xl text-2xl"}`}>
+            {i != 1? weatherData?.current[item?.value] : weatherData?.current?.condition?.text}{" "}
+            <span className="text-xl ">{i != 1 ? item?.unit : ""}</span>
             <span className={`${i == 1 ? "visible" : "hidden"}`}>
               <img
-                src={weatherData.current.condition.icon}
+                src={weatherData?.current?.condition?.icon}
                 height={50}
                 width={50}
-                className="absolute hidden xl:block top-[32vh] left[11.5vw] z-10 "
+                className="absolute hidden xl:block top-[32vh] left[11?.5vw] z-10 "
               />
               
               
@@ -242,7 +259,7 @@ const Weather = ({ city,isDayHandle }) => {
         <span className="font-normal uppercase underline font-secon">
           Location:
         </span>{" "}
-        <br /> {weatherData.location.region}, {weatherData.location.country}
+        <br /> {weatherData?.location?.region}, {weatherData?.location?.country}
         <br />
         <br />
         <span className="font-normal uppercase underline font-secon">
@@ -250,7 +267,7 @@ const Weather = ({ city,isDayHandle }) => {
         </span>{" "}
         <br />{" "}
         <span className="font-secon font-light text-lg sm:text-xl">
-          {weatherData.current.last_updated}
+          {weatherData?.current?.last_updated}
         </span>
         {(codee)!=null?<img className="absolute xl:top-[3.8vh] xl:left-[15vw] left-[17vw] top-[3.5vh] lg:inline-block hidden" src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${codee}.svg`} width="23" alt='Image 404'/>:''}
       </div>
